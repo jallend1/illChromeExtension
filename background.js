ca4638e64fe6f8da8a1a4e9f6929a83ce55ebedc
Jason Allen <jallend1@gmail.com>
2024-05-09 17:24:39 -0700

    Removes unused index and adds notes about future functionality

--- background.js
+++ background.js
@@ -1,3 +1,9 @@
+// Things to integrate with the extension
+// DONE 1. Copy the WorldShare address to clipboard (WorldShare)
+// *2. Copy the request information to clipboard (WorldShare - No context...Toggle? Keyboard CTRL-SHIFT-I?)
+// *3. Paste the request information (Evergreen - Contextual with URL staff/cat/ill/track?)
+// *4. Generate an overdue letter (Evergreen - Contextual with URL staff/circ/patron/*?)
+
 chrome.action.onClicked.addListener(async (tab) => {
     chrome.scripting.executeScript({
         target: { tabId: tab.id },
