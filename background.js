1ebac1b154c1f96fc8f59c804992c064221fe64d
Jason Allen <jallend1@gmail.com>
2024-05-16 16:25:01 -0700

    Adds functionality to copy the request data from WorldShare into the extension

--- background.js
+++ background.js
@@ -2,8 +2,8 @@
 // DONE 1. Copy the WorldShare address to clipboard (WorldShare)
 // *2. Copy the request information to clipboard (WorldShare - No context...Toggle? Keyboard CTRL-SHIFT-I?)
 // *3. Paste the request information (Evergreen - Contextual with URL staff/cat/ill/track?)
-// PARTIALLY DONE *4. Generate an overdue letter (Evergreen - Contextual with URL staff/circ/patron/*?)
-//   4a. Isolate overdueNotice function into own file
+// DONE *4. Generate an overdue letter (Evergreen - Contextual with URL staff/circ/patron/*?)
+// DONE  4a. Isolate overdueNotice function into own file
 // *5. Generate an invoice for external partners (Evergreen - Contextual with patron type or name?)
 
 chrome.commands.onCommand.addListener((command) => {
@@ -12,6 +12,13 @@ chrome.commands.onCommand.addListener((command) => {
             chrome.scripting.executeScript({
                 target: { tabId: activeTab.id },
                 files: ['./scripts/overdueNotice.js']
+            });
+        });
+    } else if(command === 'copyFromOCLC') {
+        chrome.tabs.query({ active: true, currentWindow: true }, ([activeTab]) => {
+            chrome.scripting.executeScript({
+                target: { tabId: activeTab.id },
+                files: ['./scripts/copyFromOCLC.js']
             });
         });
     }
