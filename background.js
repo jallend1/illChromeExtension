b7752abaae8c6d46e5d8b2a3512a4e73ed9f7b67
Jason Allen <jallend1@gmail.com>
2024-05-21 17:11:07 -0700

    Refactors onCommand listeners to take advantage of command object

--- background.js
+++ background.js
@@ -7,47 +7,9 @@
 // *5. Generate an invoice for external partners (Evergreen - Contextual with patron type or name?)
 // *6. Automatically switch tabs from request screen to Evergreen tab and auto-fill?
 // DONE *7 Add an HTML file detailing all the shortcuts
-// *8 Add a context menu for right-clicking on page and revealing custom options?
+// DONE *8 Add a context menu for right-clicking on page and revealing custom options?
 // *9 Move copy from WorldShare into command palette for consistency?
 // * 10 Move state abbreviation conversion into its own file because it's now being used by multiple sources
-
-chrome.commands.onCommand.addListener((command) => {
-  // Generates overdue notice for patron if there are ILLs overdue on patron's account
-  // Requires having the list of checked out items open on screen
-  if (command === "generate_overdue") {
-    chrome.tabs.query({ active: true, currentWindow: true }, ([activeTab]) => {
-      chrome.scripting.executeScript({
-        target: { tabId: activeTab.id },
-        files: ["./scripts/overdueNotice.js"],
-      });
-    });
-  }
-  // Copies the request data (Title, patron barcode, lending library address) from the WorldShare request and stores it in the clipboard
-  else if (command === "copyFromOCLC") {
-    chrome.tabs.query({ active: true, currentWindow: true }, ([activeTab]) => {
-      chrome.scripting.executeScript({
-        target: { tabId: activeTab.id },
-        files: ["./scripts/copyFromOCLC.js"],
-      });
-    });
-  } else if (command === "copyAddressFromWorldShare") {
-    chrome.tabs.query({ active: true, currentWindow: true }, ([activeTab]) => {
-      chrome.scripting.executeScript({
-        target: { tabId: activeTab.id },
-        files: ["./scripts/copyWorldShareAddress.js"],
-      });
-    });
-  }
-  // Parses the copied request data from the copyFromOCLC command and pastes it into the Evergreen ILL request form
-  else if (command === "pasteToEvergreen") {
-    chrome.tabs.query({ active: true, currentWindow: true }, ([activeTab]) => {
-      chrome.scripting.executeScript({
-        target: { tabId: activeTab.id },
-        files: ["./scripts/pasteToEvergreen.js"],
-      });
-    });
-  }
-});
 
 const currentOptions = [
   { id: "copyWorldShareAddress", title: "Copy Address from WorldShare" },
@@ -56,6 +18,24 @@ const currentOptions = [
   { id: "overdueNotice", title: "Generate Overdue Notice" },
 ];
 
+// Add keyboard shortcuts for each option
+chrome.commands.onCommand.addListener((command) => {
+  currentOptions.forEach((option) => {
+    if (command === option.id) {
+      chrome.tabs.query(
+        { active: true, currentWindow: true },
+        ([activeTab]) => {
+          chrome.scripting.executeScript({
+            target: { tabId: activeTab.id },
+            files: [`./scripts/${option.id}.js`],
+          });
+        }
+      );
+    }
+  });
+});
+
+// Add context menu items for each option
 chrome.runtime.onInstalled.addListener(() => {
   currentOptions.forEach((option) => {
     chrome.contextMenus.create({
@@ -66,6 +46,7 @@ chrome.runtime.onInstalled.addListener(() => {
   });
 });
 
+// Add event listener for context menu clicks
 chrome.contextMenus.onClicked.addListener((item) => {
   chrome.tabs.query({ active: true, currentWindow: true }, ([activeTab]) => {
     console.log(item);
--- popup/popup.js
+++ popup/popup.js
@@ -1,9 +1,10 @@
+// We don't have this rocking yet. Aspirational!
+
 console.log("popup.js");
 const buttons = document.querySelectorAll("td");
 buttons.forEach((button) => {
   button.addEventListener("click", (event) => {
     const buttonId = event.target.id;
     console.log(buttonId);
-    // chrome.runtime.sendMessage({ command: buttonId });
   });
 });
