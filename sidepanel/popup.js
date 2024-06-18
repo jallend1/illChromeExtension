b4adceed4ba8ca1e5bd3bcdee40130337054b087
Jason Allen <jallend1@gmail.com>
2024-06-18 12:46:47 -0700

    Implements basic functionality for sidebar

---
+++ sidepanel/popup.js
@@ -0,0 +1,17 @@
+const buttons = document.querySelectorAll("button");
+
+const initiateScript = (scriptName) => {
+  chrome.runtime.sendMessage(
+    { command: scriptName, data: scriptName },
+    function (response) {
+      console.log(response);
+    }
+  );
+};
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    const buttonId = event.target.id;
+    initiateScript(buttonId);
+  });
+});
