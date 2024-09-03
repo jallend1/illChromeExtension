ebf14969f311316e96c8af782ada4a18bb8b424d
Jason Allen <jallend1@gmail.com>
2024-09-03 16:22:24 -0700

    Begins implementation of error modal for there is no data to paste to Evergreen

--- scripts/pasteToEvergreen.js
+++ scripts/pasteToEvergreen.js
@@ -58,6 +58,50 @@ function pasteToEvergreen() {
     });
   };
 
+  // TODO: Finish implementing an error modal for when the data is not found in local storage
+
+  const statusModal = (data, backgroundColor, imgURL) => {
+    let imgURL = chrome.runtime.getURL("images/kawaii-book-sad.png");
+    let headerColor = "#e85e6a";
+    let result = `<h2 style="font-weight: thin; padding: 1rem; color: #3b607c">Error!</h2> <p style="font-size: 1rem;">"${err}";</p>`;
+    const modal = document.createElement("div");
+    modal.setAttribute("id", "modal");
+    modal.setAttribute(
+      "style",
+      `
+      position: fixed;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      border-radius: 1rem;
+      z-index: 1000;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      color: #000;
+      font-size: 4rem;
+      border: 1px solid #000;
+      box-shadow: 0 0 10px 5px #000;
+    `
+    );
+    modal.innerHTML = `
+    <div>  
+    <div style="background-color: ${backgroundColor}; padding: 1rem; border-radius: 1rem 1rem 0 0; text-align: center;">
+    <img src=${imgURL} style="width: 100px; height: 100px; border-radius: 50%;">
+    </div>
+    <div style="background-color: #f9f9f9;  text-align: center; border-radius: 0 0 1rem 1rem; padding: 1rem;">
+    ${data}
+    </div>
+    </div>
+    `;
+
+    document.body.appendChild(modal);
+    setTimeout(() => {
+      modal.remove();
+    }, 3000);
+  };
+
   // TODO: Pressing enter seems to submit the form, which I don't think is the demonstrated behavior on local client
   // document.addEventListener("keydown", (e) => {
   //   if (e.key === "Enter") {
