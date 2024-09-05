802f512bf1159c1a8ed4a941eb99c51e60f3eb91
Jason Allen <jallend1@gmail.com>
2024-09-05 16:33:50 -0700

    Modifies lendingAlert to also highlight error on place hold screens that frequently get overlooked

--- scripts/lendingFeeAlert.js
+++ scripts/lendingFeeAlert.js
@@ -91,3 +91,58 @@ function lendingFeeAlert() {
 }
 
 lendingFeeAlert();
+
+// TODO: Break out into its own content script if behavior is needed on other pages
+
+function emphasizeErrors() {
+  const form = document.querySelector("form");
+  form.addEventListener("submit", checkAndEmphasizeHoldStatus);
+}
+
+function checkAndEmphasizeHoldStatus() {
+  let holdStatus = document.querySelector(".alert.alert-danger");
+  if (!holdStatus) {
+    let attempts = 0;
+    const checkHoldStatus = () => {
+      if (attempts < 15) {
+        setTimeout(() => {
+          holdStatus = document.querySelector(".alert.alert-danger");
+          if (!holdStatus) {
+            attempts++;
+            checkHoldStatus();
+          } else {
+            const textContent = holdStatus.textContent
+              .replaceAll("_", " ")
+              .trim()
+              .toLowerCase();
+            const modifiedText =
+              "Error: " +
+              textContent.charAt(0).toUpperCase() +
+              textContent.slice(1) +
+              ". Override required.";
+
+            const holdStatusStyle = {
+              backgroundColor: "red",
+              color: "white",
+              fontSize: "1.15rem",
+              textWrap: "wrap",
+              textAlign: "center",
+              borderRadius: "10px",
+            };
+
+            Object.entries(holdStatusStyle).forEach(([key, value]) => {
+              holdStatus.style[key] = value;
+            });
+
+            holdStatus.textContent = modifiedText;
+          }
+        }, 100);
+      } else {
+        console.log("Hold status not updated after 15 seconds");
+      }
+    };
+    checkHoldStatus();
+  }
+}
+
+setTimeout(emphasizeErrors, 2000);
