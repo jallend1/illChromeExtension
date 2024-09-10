d1dcd587d4f770c827a3f3e7b27098a66e583a51
Jason Allen <jallend1@gmail.com>
2024-09-10 14:15:34 -0700

    Refactors pasteToEvergreen to utilize the modal module

--- scripts/pasteToEvergreen.js
+++ scripts/pasteToEvergreen.js
@@ -1,132 +1,100 @@
-function pasteToEvergreen() {
-  const updateInputField = (selector, value, prefix = "") => {
-    const inputField = document.querySelector(selector);
-    if (!inputField) {
-      statusModal(
-        "Couldn't find the right spots to insert the request information. Make sure you're on the 'Create ILL Screen.' If the problems continue, contact Jason."
-      );
-      return;
-    }
-    inputField.value = prefix + value;
-    const event = new Event("input", {
-      bubbles: true,
-      cancelable: true,
-    });
-    inputField.dispatchEvent(event);
-  };
+(async () => {
+  const { statusModal } = await import(
+    chrome.runtime.getURL("modules/modal.js")
+  );
 
-  const updateTitle = (title) => {
-    updateInputField("#title-input", title, "ILL Title - ");
-  };
+  function pasteToEvergreen() {
+    let result = `<h2 style="font-weight: thin; padding: 1rem; color: #3b607c">Something went wrong!</h2> <p style="font-size: 1rem;">Couldn't find the right spots to insert the request information. Make sure you're on the 'Create ILL Screen.' If the problems continue, contact Jason.</p>`;
+    const imgURL = chrome.runtime.getURL("images/kawaii-book-sad.png");
+    const headerColor = "#e85e6a";
 
-  const updateCallNumber = (requestNumber) => {
-    updateInputField("#callnumber-input", requestNumber, "IL");
-  };
-
-  const updatePatronBarcode = (patronID) => {
-    updateInputField("#patron-barcode-input", patronID);
-  };
-
-  const updatePatronAddress = (addressString) => {
-    const inputField = document.querySelector("textarea");
-    if (!inputField) {
-      statusModal(
-        "Couldn't find the right spots to insert the request information. Make sure you're on the 'Create ILL Screen.' If the problems continue, contact Jason."
-      );
-      return;
-    }
-    const bagText = "**BAG**\n";
-    const boxText = "**RETURN IN BOX**\n";
-    if (inputField.value.includes(bagText)) {
-      addressString = bagText + addressString;
-    }
-    if (inputField.value.includes(boxText)) {
-      addressString = boxText + addressString;
-    }
-    updateInputField("textarea", addressString);
-  };
-
-  const extractArrayFromLocalStorage = () => {
-    chrome.storage.local.get("requestData", (result) => {
-      if (!result.requestData) {
-        statusModal(
-          "Couldn't find any data to paste! Try copying from the WorldShare request again. If the error persists, please contact Jason."
-        );
+    const updateInputField = (selector, value, prefix = "") => {
+      const inputField = document.querySelector(selector);
+      if (!inputField) {
+        statusModal(result, headerColor, imgURL);
         return;
-      } else {
-        const { addressString, requestNumber, title, patronID, isLendingFee } =
-          JSON.parse(result.requestData);
-        updateTitle(title);
-        updateCallNumber(requestNumber);
-        updatePatronBarcode(patronID);
-        updatePatronAddress(addressString);
-        if (isLendingFee !== "0.00" && isLendingFee !== "") {
-          alert(
-            `This request might have a lending fee of $${isLendingFee}. Please verify and add it to the patron's record.`
-          );
-        }
-        // TODO: Seems impossible to focus on the item barcode from sidebar but functions as expected with keyboard shortcut?
-        // const kclsBarcodeInput = document.querySelector("#item-barcode-input");
-        // kclsBarcodeInput.focus();
       }
-    });
-  };
-
-  const statusModal = (err) => {
-    let imgURL = chrome.runtime.getURL("images/kawaii-book-sad.png");
-    let headerColor = "#e85e6a";
-    let result = `<h2 style="font-weight: thin; padding: 1rem; color: #3b607c">Error</h2> <p style="font-size: 1rem;">${err}</p>`;
-    const modal = document.createElement("div");
-    modal.setAttribute("id", "modal");
-    const modalStyle = {
-      position: "fixed",
-      top: "50%",
-      left: "50%",
-      transform: "translate(-50%, -50%)",
-      borderRadius: "1rem",
-      zIndex: "1000",
-      display: "flex",
-      flexDirection: "column",
-      justifyContent: "center",
-      alignItems: "center",
-      color: "#000",
-      fontSize: "4rem",
-      border: "1px solid #000",
-      boxShadow: "0 0 10px 5px #000",
+      inputField.value = prefix + value;
+      const event = new Event("input", {
+        bubbles: true,
+        cancelable: true,
+      });
+      inputField.dispatchEvent(event);
     };
-    for (const [key, value] of Object.entries(modalStyle)) {
-      modal.style[key] = value;
-    }
 
-    modal.innerHTML = `
-    <div>  
-      <div style="background-color: ${headerColor}; padding: 1rem; border-radius: 1rem 1rem 0 0; text-align: center;">
-        <img src=${imgURL} style="width: 100px; height: 100px; border-radius: 50%;">
-      </div>
-      <div style="background-color: #f9f9f9;  text-align: center; border-radius: 0 0 1rem 1rem; padding: 1rem;">
-        ${result}
-      </div>
-    </div>
-    `;
+    const updateTitle = (title) => {
+      updateInputField("#title-input", title, "ILL Title - ");
+    };
 
-    document.body.appendChild(modal);
-    setTimeout(() => {
-      modal.remove();
-    }, 4000);
-  };
+    const updateCallNumber = (requestNumber) => {
+      updateInputField("#callnumber-input", requestNumber, "IL");
+    };
 
-  // TODO: Pressing enter seems to submit the form, which I don't think is the demonstrated behavior on local client
-  // document.addEventListener("keydown", (e) => {
-  //   if (e.key === "Enter") {
-  //     const buttons = document.querySelectorAll("button");
-  //     buttons.forEach((button) => {
-  //       if (button.textContent === "Create Item") {
-  //         button.focus();
-  //       }
-  //     });
-  //   }
-  // });
+    const updatePatronBarcode = (patronID) => {
+      updateInputField("#patron-barcode-input", patronID);
+    };
 
-  extractArrayFromLocalStorage();
-}
-pasteToEvergreen();
+    const updatePatronAddress = (addressString) => {
+      const inputField = document.querySelector("textarea");
+      if (!inputField) {
+        statusModal(result, headerColor, imgURL);
+        return;
+      }
+      const bagText = "**BAG**\n";
+      const boxText = "**RETURN IN BOX**\n";
+      if (inputField.value.includes(bagText)) {
+        addressString = bagText + addressString;
+      }
+      if (inputField.value.includes(boxText)) {
+        addressString = boxText + addressString;
+      }
+      updateInputField("textarea", addressString);
+    };
+
+    const extractArrayFromLocalStorage = () => {
+      chrome.storage.local.get("requestData", (result) => {
+        if (!result.requestData) {
+          result = `<h2 style="font-weight: thin; padding: 1rem; color: #3b607c">Something went wrong!</h2> <p style="font-size: 1rem;">Couldn't find any data to paste! Try copying from the WorldShare request again. If the error persists, please contact Jason.</p>`;
+
+          statusModal(result, headerColor, imgURL);
+          return;
+        } else {
+          const {
+            addressString,
+            requestNumber,
+            title,
+            patronID,
+            isLendingFee,
+          } = JSON.parse(result.requestData);
+          updateTitle(title);
+          updateCallNumber(requestNumber);
+          updatePatronBarcode(patronID);
+          updatePatronAddress(addressString);
+          if (isLendingFee !== "0.00" && isLendingFee !== "") {
+            alert(
+              `This request might have a lending fee of $${isLendingFee}. Please verify and add it to the patron's record.`
+            );
+          }
+          // TODO: Seems impossible to focus on the item barcode from sidebar but functions as expected with keyboard shortcut?
+          // const kclsBarcodeInput = document.querySelector("#item-barcode-input");
+          // kclsBarcodeInput.focus();
+        }
+      });
+    };
+
+    // TODO: Pressing enter seems to submit the form, which I don't think is the demonstrated behavior on local client
+    // document.addEventListener("keydown", (e) => {
+    //   if (e.key === "Enter") {
+    //     const buttons = document.querySelectorAll("button");
+    //     buttons.forEach((button) => {
+    //       if (button.textContent === "Create Item") {
+    //         button.focus();
+    //       }
+    //     });
+    //   }
+    // });
+
+    extractArrayFromLocalStorage();
+  }
+  pasteToEvergreen(statusModal);
+})();
