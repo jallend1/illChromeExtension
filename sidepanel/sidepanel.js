3556baba4bd8d58d83f1dd83bfe15abb34ddae4f
Jason Allen <jallend1@gmail.com>
2024-09-26 15:59:37 -0700

    Removes vestigial error modals and unuused details buttons

--- sidepanel/sidepanel.js
+++ sidepanel/sidepanel.js
@@ -18,25 +18,6 @@ const initiateScript = (scriptName) => {
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     var currentTab = tabs[0];
     chrome.tabs.update(currentTab.id, { active: true });
-    // Displays error message if user is not on a WorldShare page and runs a WorldShare Script
-    // if (
-    //   !currentTab.url.includes("kingcountylibrarysystem") &&
-    //   (scriptName === "copyFromOCLC" || scriptName === "copyWorldShareAddress")
-    // ) {
-    //   errorModal("This only works from a WorldShare page.");
-    //   return;
-    // }
-    // // Displays error if running an Evergreen script outside Evergreen
-    // else if (
-    //   !currentTab.url.includes("kcls.org") &&
-    //   scriptName === "pasteToEvergreen"
-    // ) {
-    //   errorModal(
-    //     "Please run this from the 'Create New ILL' screen in Evergreen."
-    //   );
-    //   return;
-    // } else {
-    // Sends message to background.js to run the script
     chrome.runtime.sendMessage(
       { command: scriptName, data: scriptName },
       async (response) => {
@@ -66,109 +47,6 @@ const extractFromStorage = async (key) => {
       console.error(`Failed to copy ${key} to clipboard`, error);
     }
   }
-};
-
-const moreInfoModalData = (data) => {
-  const generateHelpContent = (title, description, steps) => `
-    <h2>${title}</h2>
-    <section class="steps">
-      <p>${description}</p>
-      <h3>Steps:</h3>
-      <ol class="steps">
-        ${steps.map((step) => `<li>${step}</li>`).join("")}
-      </ol>
-    </section>
-`;
-
-  const helpContent = {
-    "copy-help": generateHelpContent(
-      "Copy from OCLC",
-      "This copies all the important WorldShare information from the request so it can be entered into Evergreen.",
-      [
-        "Go to the requests's page in WorldShare.",
-        "Click the 'Copy Request Data from WorldShare' button.",
-        "The request data is now saved in your browser! To unleash it, see the 'Paste the Request Data in Evergreen' button details for next steps.",
-      ]
-    ),
-    "paste-help": generateHelpContent(
-      "Paste to Evergreen",
-      "This pastes all the information that was extracted from the 'Copy Request Data' button into 'Create ILL' screen in Evergreen.",
-      [
-        "Copy the request data from WorldShare using the 'Copy Request Data from WorldShare' button.",
-        "Navigate to the 'Create ILL' screen in Evergreen. (Alt+I)",
-        "Click the button and verify the information matches the request.",
-      ]
-    ),
-    "address-help": generateHelpContent(
-      "Copy WorldShare Address",
-      "The WorldShare address field isn't formatted for easy cut and pasting. This resolves that!",
-      [
-        "Have the WorldShare request open.",
-        "Click the Copy WorldShare Address button.",
-        "Navigate to the Dymo program and hit Ctrl+V to paste the address.",
-      ]
-    ),
-    "overdue-help": generateHelpContent(
-      "Generate Overdue Letter",
-      "Pulls title and due date information from a patron's record and inserts it into a letter.",
-      [
-        "Open the patron's record to the 'Items Out' screen.",
-        "Click the Generate Overdue Letter button.",
-        "Navigate to a new email and hit Ctrl+V to paste the letter.",
-      ]
-    ),
-    "update-account-help": generateHelpContent(
-      "Update ILL Account Address",
-      "Library accounts expire and need updating on the regular. This button takes care of the defaults.",
-      [
-        "Navigate to the Edit Account screen in Evergreen for the desired account.",
-        "Click the Update ILL Account Address button.",
-        "Button will automatically fill in an adult birthday, correct patron type, and district of residence.",
-      ]
-    ),
-  };
-
-  return helpContent[data] || "Details not yet available.";
-};
-
-const showModal = (title, content, isError = false) => {
-  const modal = document.createElement("div");
-  modal.setAttribute("id", "modal");
-  modal.setAttribute(
-    "style",
-    `
-    color: ${isError ? "#fff" : "#000"};
-    border: 1px solid ${isError ? "#f00" : "#000"};
-    box-shadow: 0 0 10px 5px ${isError ? "#f00" : "#000"};
-    background-color: ${isError ? "#f00" : "#f9f9f9"};
-  `
-  );
-
-  const modalTitle = document.createElement("h2");
-  modalTitle.textContent = title;
-  modal.appendChild(modalTitle);
-
-  const modalContent = document.createElement("div");
-  modalContent.innerHTML = content;
-  modal.appendChild(modalContent);
-
-  const closeButton = document.createElement("button");
-  closeButton.textContent = "Close";
-  closeButton.addEventListener("click", () => {
-    document.body.removeChild(modal);
-  });
-  modal.appendChild(closeButton);
-
-  document.body.appendChild(modal);
-};
-
-const moreInfoModal = (buttonId) => {
-  const data = moreInfoModalData(buttonId);
-  showModal("More Information", data);
-};
-
-const errorModal = (errorMessage) => {
-  showModal("Error", errorMessage, true);
 };
 
 const addEventListeners = () => {
@@ -216,13 +94,6 @@ const addEventListeners = () => {
     body.classList.toggle("fun-mode", isVibrantMode);
     body.classList.toggle("dreary-mode", !isVibrantMode);
     modeToggle.textContent = isVibrantMode ? "Evergreen Mode" : "Vibrant Mode";
-  });
-
-  moreInfoButtons.forEach((button) => {
-    button.addEventListener("click", (event) => {
-      const buttonId = event.target.id;
-      moreInfoModal(buttonId);
-    });
   });
 
   lendingMode.addEventListener("click", () => {
