d20f03e31544d65db2f82dfa71643099916d3e4b
Jason Allen <jallend1@gmail.com>
2024-10-01 13:52:29 -0700

    If no ISBN is present, extracts title and/or author and populates search with acceptably formatted text

--- scripts/isbnSearch.js
+++ scripts/isbnSearch.js
@@ -1,21 +1,52 @@
 function isbnSearch() {
-  // WorldShare keeps all requests open, so extracts the latest ISBN field
-  const isbnFields = document.querySelectorAll(".yui-field-isbn");
-  const latestIsbnField = isbnFields[isbnFields.length - 1];
-  const isbnContent = latestIsbnField.textContent;
-  const firstISBN = isbnContent.split(" ")[0];
+  // TODO: Adapt for copy requests
 
-  // Checks previousISBN to prevent duplicate searches
-  const previousIsbn = sessionStorage.getItem("isbn");
-  // TODO: Maybe make this a bit more expansive? Error modal? Automatic page refresh?
-  if (previousIsbn === firstISBN) {
-    alert(
-      "This ISBN matches the last one we searched. Maybe do a page refresh! Or at least double check."
-    );
+  const extractFields = (selector) => {
+    const fields = document.querySelectorAll(selector);
+    // Title field has an extra node for some reason
+    const subtractor = selector.includes("title") ? 2 : 1;
+    const latestField = fields[fields.length - subtractor];
+    if (!latestField) return null;
+    return latestField.textContent;
+  };
+
+  let isbn = extractFields(".yui-field-isbn");
+  let title = extractFields(".yui-field-title");
+  let author = extractFields(".yui-field-author");
+
+  let searchQuery = null;
+  if (isbn) {
+    // Takes only the first ISBN if multiple are present
+    searchQuery = isbn.split(" ")[0];
+  } else if (title && author) {
+    searchQuery = `${title} ${author.split(";", 0)}`;
+  } else if (title) {
+    searchQuery = title;
+  } else {
+    alert("No ISBN or Title/Author found. Please check the record.");
+    return;
   }
-  sessionStorage.setItem("isbn", firstISBN);
-  const urlSuffix = `search?org=1&limit=10&query=${firstISBN}%20&fieldClass=keyword&joinOp=&matchOp=contains&dateOp=is&ridx=122`;
-  chrome.runtime.sendMessage({ action: "isbnSearch", url: urlSuffix });
+
+  if (searchQuery) {
+    // Keyword search doesn't like colons
+    searchQuery = searchQuery.replace(":", "");
+
+    // Checks previous isbnSearch to prevent duplicate searches
+    const previousIsbnSearch = sessionStorage.getItem("isbnSearch");
+
+    // TODO: Maybe make this a bit more expansive? Error modal? Automatic page refresh?
+    if (previousIsbnSearch === searchQuery) {
+      alert(
+        "This matches the last search we did. Double check the item is what you're looking for on the next page. If not, try refreshing WorldShare!"
+      );
+    }
+    sessionStorage.setItem("isbnSearch", searchQuery);
+
+    const urlSuffix = `search?org=1&limit=10&query=${searchQuery}%20&fieldClass=keyword&joinOp=&matchOp=contains&dateOp=is&ridx=122`;
+    chrome.runtime.sendMessage({ action: "isbnSearch", url: urlSuffix });
+  } else {
+    alert("No search eligible search parameters found on this page.");
+  }
 }
 
 isbnSearch();
