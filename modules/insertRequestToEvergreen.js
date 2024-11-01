7c053dd1d8dd2c821d65e7dd72d292a092328b46
Jason Allen <jallend1@gmail.com>
2024-11-01 15:42:01 -0700

    Automatically inserts request data onto Create ILL Item screen if its detected in local storage

---
+++ modules/insertRequestToEvergreen.js
@@ -0,0 +1,95 @@
+// Description: Inserts request data copied from WorldShare into Evergreen ILL form if it's detected in local storage
+// TODO: Almost an exact copy of pasteToEvergreen.js -- If successful, refactor to use a single function
+export async function insertRequestToEvergreen() {
+  const { statusModal } = await import(
+    chrome.runtime.getURL("modules/modal.js")
+  );
+
+  // TODO: Patron name stored in localStorage but not used in this function yet
+  // Use to 1) Verify patron name on Evergreen screen 2) Add patron pickup location
+  function pasteToEvergreen() {
+    let result = `<h2 style="font-weight: thin; padding: 1rem; color: #3b607c">Something went wrong!</h2> <p style="font-size: 1rem;">Couldn't find the right spots to insert the request information. Make sure you're on the 'Create ILL Screen.' If the problems continue, contact Jason.</p>`;
+    const imgURL = chrome.runtime.getURL("images/kawaii-book-sad.png");
+    const headerColor = "#e85e6a";
+
+    const updateInputField = (selector, value, prefix = "") => {
+      const inputField = document.querySelector(selector);
+      if (!inputField) {
+        statusModal(result, headerColor, imgURL);
+        return;
+      }
+      inputField.value = prefix + value;
+      const event = new Event("input", {
+        bubbles: true,
+        cancelable: true,
+      });
+      inputField.dispatchEvent(event);
+    };
+
+    const updateTitle = (title) => {
+      updateInputField("#title-input", title, "ILL Title - ");
+    };
+
+    const updateCallNumber = (requestNumber) => {
+      updateInputField("#callnumber-input", requestNumber, "IL");
+    };
+
+    const updatePatronBarcode = (patronID) => {
+      updateInputField("#patron-barcode-input", patronID);
+    };
+
+    const updatePatronAddress = (addressString) => {
+      const inputField = document.querySelector("textarea");
+      if (!inputField) {
+        statusModal(result, headerColor, imgURL);
+        return;
+      }
+      const bagText = "**BAG**\n";
+      const boxText = "**RETURN IN BOX**\n";
+      if (inputField.value.includes(bagText)) {
+        addressString = bagText + addressString;
+      }
+      if (inputField.value.includes(boxText)) {
+        addressString = boxText + addressString;
+      }
+      updateInputField("textarea", addressString);
+    };
+
+    const extractArrayFromLocalStorage = () => {
+      chrome.storage.local.get("requestData", (result) => {
+        if (!result.requestData) {
+          // TODO: Something less obtrusive than the statusModal?
+          // TODO: Default behavior shouldn't be met with annoyances, even though I'm not sure when it would ever need to occur
+          result = `<h2 style="font-weight: thin; padding: 1rem; color: #3b607c">No stored request data found.</h2> <p style="font-size: 1rem;">Couldn't find any data to insert onto the page.</p>`;
+          statusModal(result, headerColor, imgURL);
+          return;
+        } else {
+          const {
+            addressString,
+            requestNumber,
+            title,
+            patronID,
+            isLendingFee,
+          } = JSON.parse(result.requestData);
+          updateTitle(title);
+          updateCallNumber(requestNumber);
+          updatePatronBarcode(patronID);
+          updatePatronAddress(addressString);
+          if (isLendingFee !== "0.00" && isLendingFee !== "") {
+            alert(
+              `This request might have a lending fee of $${isLendingFee}. Please verify and add it to the patron's record.`
+            );
+          }
+          // TODO: Seems impossible to focus on the item barcode from sidebar but functions as expected with keyboard shortcut?
+          const kclsBarcodeInput = document.querySelector(
+            "#item-barcode-input"
+          );
+          kclsBarcodeInput.focus();
+        }
+      });
+    };
+
+    extractArrayFromLocalStorage();
+  }
+  pasteToEvergreen(statusModal);
+}
--- scripts/createILLPageMods.js
+++ scripts/createILLPageMods.js
@@ -1,134 +1,155 @@
-function createILLPageMods() {
-  const applyStyles = (element, styles) => {
-    for (const property in styles) {
-      element.style[property] = styles[property];
-    }
-  };
+(async () => {
+  const { insertRequestToEvergreen } = await import(
+    chrome.runtime.getURL("modules/insertRequestToEvergreen.js")
+  );
 
-  const removeExistingCheckboxContainer = () => {
-    const existingCheckboxContainer = document.querySelector(
-      "#checkbox-container"
-    );
-    if (existingCheckboxContainer) {
-      existingCheckboxContainer.remove();
-    }
-  };
+  insertRequestToEvergreen();
 
-  const addILLCheckboxes = (checkboxId, labelText, textToPrepend) => {
-    const checkboxContainer = document.querySelector("#checkbox-container");
-
-    // Create container div for the checkbox and label
-    const div = document.createElement("div");
-    const divStyles = {
-      display: "flex",
-      alignItems: "center",
-    };
-    applyStyles(div, divStyles);
-
-    // Create the checkbox element
-    const checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
-    checkbox.id = checkboxId;
-    const checkboxStyles = {
-      marginLeft: "10px",
-      marginRight: "5px",
-    };
-    applyStyles(checkbox, checkboxStyles);
-
-    // Adds listener to the checkbox to prepend the text to the address field
-    checkbox.addEventListener("click", (e) => {
-      const addressField = document.querySelector("textarea");
-      if (e.target.checked) {
-        addressField.value = textToPrepend + addressField.value;
-      } else {
-        addressField.value = addressField.value.replace(textToPrepend, "");
+  function createILLPageMods() {
+    const applyStyles = (element, styles) => {
+      for (const property in styles) {
+        element.style[property] = styles[property];
       }
-      const inputEvent = new Event("input", {
-        bubbles: true,
-        cancelable: true,
-      });
-      addressField.dispatchEvent(inputEvent);
-    });
-
-    // Create the label
-    const label = document.createElement("label");
-    label.style.fontSize = "1.5em";
-    label.htmlFor = checkboxId;
-    label.appendChild(document.createTextNode(labelText));
-
-    // Append checkbox and label to the div
-    div.appendChild(checkbox);
-    div.appendChild(label);
-
-    // Adds the checkbox div to the form
-    checkboxContainer.appendChild(div);
-  };
-
-  // Creates a parent div for the ILL custom checkboxes
-  const createCheckboxContainer = () => {
-    removeExistingCheckboxContainer();
-    const parentILLForm = document.querySelector(".form-validated");
-    const checkboxContainer = document.createElement("div");
-    checkboxContainer.id = "checkbox-container";
-
-    // Styles for the checkbox container
-    const styles = {
-      backgroundColor: "#fff",
-      padding: "1rem",
-      borderRadius: "5px",
-      border: "1px solid #701d9d",
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      flexDirection: "column",
-      marginTop: "1rem",
     };
 
-    applyStyles(checkboxContainer, styles);
+    const removeExistingCheckboxContainer = () => {
+      const existingCheckboxContainer = document.querySelector(
+        "#checkbox-container"
+      );
+      if (existingCheckboxContainer) {
+        existingCheckboxContainer.remove();
+      }
+    };
 
-    parentILLForm.appendChild(checkboxContainer);
+    // const createClearFormButton = () => {
+    //   const clearFormButton = document.createElement("button");
+    //   clearFormButton.textContent = "Clear Form";
+    //   clearFormButton.style.marginTop = "1rem";
+    //   clearFormButton.style.padding = "0.5rem 1rem";
+    //   clearFormButton.style.border = "none";
+    //   clearFormButton.style.borderRadius = "0.5rem";
+    //   clearFormButton.style.backgroundColor = "#701d9d";
+    //   clearFormButton.style.color = "#fff";
+    //   clearFormButton.style.fontSize = "1.5em";
 
-    // Add checkboxes to the container
-    addILLCheckboxes("ill-bag-checkbox", "ILL came with a bag", "**BAG**\n");
-    addILLCheckboxes(
-      "ill-box-checkbox",
-      "ILL should be returned in a box",
-      "**RETURN IN BOX**\n"
-    );
-  };
+    //   clearFormButton.addEventListener("click", () => {
+    //     document.querySelector("textarea").value = "";
+    //     document.querySelector("#ill-bag-checkbox").checked = false;
+    //     document.querySelector("#ill-box-checkbox").checked = false;
+    //   });
 
-  removeExistingCheckboxContainer();
-  createCheckboxContainer();
+    //   document.querySelector(".form-validated").appendChild(clearFormButton);
+    // };
 
-  const checkForForm = setInterval(() => {
-    if (document.querySelector(".form-validated")) {
-      createCheckboxContainer();
-      monitorTextarea();
-      clearInterval(checkForForm);
-    }
-  }, 1000);
+    const addILLCheckboxes = (checkboxId, labelText, textToPrepend) => {
+      const checkboxContainer = document.querySelector("#checkbox-container");
 
-  const monitorTextarea = () => {
-    const addressField = document.querySelector("textarea");
-    addressField.addEventListener("input", () => {
-      const bagCheckbox = document.querySelector("#ill-bag-checkbox");
-      const boxCheckbox = document.querySelector("#ill-box-checkbox");
-      addressField.value.includes("**BAG**")
-        ? (bagCheckbox.checked = true)
-        : (bagCheckbox.checked = false);
-      addressField.value.includes("**RETURN IN BOX**")
-        ? (boxCheckbox.checked = true)
-        : (boxCheckbox.checked = false);
-    });
-  };
-}
+      // Create container div for the checkbox and label
+      const div = document.createElement("div");
+      const divStyles = {
+        display: "flex",
+        alignItems: "center",
+      };
+      applyStyles(div, divStyles);
 
-createILLPageMods();
+      // Create the checkbox element
+      const checkbox = document.createElement("input");
+      checkbox.type = "checkbox";
+      checkbox.id = checkboxId;
+      const checkboxStyles = {
+        marginLeft: "10px",
+        marginRight: "5px",
+      };
+      applyStyles(checkbox, checkboxStyles);
 
-// chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-//   if (request.data === "illPageMods") {
-//     createILLPageMods();
-//     sendResponse({ response: "Message received" });
-//   }
-//   return true;
-// });
+      // Adds listener to the checkbox to prepend the text to the address field
+      checkbox.addEventListener("click", (e) => {
+        const addressField = document.querySelector("textarea");
+        if (e.target.checked) {
+          addressField.value = textToPrepend + addressField.value;
+        } else {
+          addressField.value = addressField.value.replace(textToPrepend, "");
+        }
+        const inputEvent = new Event("input", {
+          bubbles: true,
+          cancelable: true,
+        });
+        addressField.dispatchEvent(inputEvent);
+      });
+
+      // Create the label
+      const label = document.createElement("label");
+      label.style.fontSize = "1.5em";
+      label.htmlFor = checkboxId;
+      label.appendChild(document.createTextNode(labelText));
+
+      // Append checkbox and label to the div
+      div.appendChild(checkbox);
+      div.appendChild(label);
+
+      // Adds the checkbox div to the form
+      checkboxContainer.appendChild(div);
+    };
+
+    // Creates a parent div for the ILL custom checkboxes
+    const createCheckboxContainer = () => {
+      removeExistingCheckboxContainer();
+      const parentILLForm = document.querySelector(".form-validated");
+      const checkboxContainer = document.createElement("div");
+      checkboxContainer.id = "checkbox-container";
+
+      // Styles for the checkbox container
+      const styles = {
+        backgroundColor: "#fff",
+        padding: "1rem",
+        borderRadius: "5px",
+        border: "1px solid #701d9d",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        marginTop: "1rem",
+      };
+
+      applyStyles(checkboxContainer, styles);
+
+      parentILLForm.appendChild(checkboxContainer);
+
+      // Add checkboxes to the container
+      addILLCheckboxes("ill-bag-checkbox", "ILL came with a bag", "**BAG**\n");
+      addILLCheckboxes(
+        "ill-box-checkbox",
+        "ILL should be returned in a box",
+        "**RETURN IN BOX**\n"
+      );
+    };
+
+    removeExistingCheckboxContainer();
+    createCheckboxContainer();
+
+    const checkForForm = setInterval(() => {
+      if (document.querySelector(".form-validated")) {
+        createCheckboxContainer();
+        // createClearFormButton();
+        monitorTextarea();
+        clearInterval(checkForForm);
+      }
+    }, 1000);
+
+    const monitorTextarea = () => {
+      const addressField = document.querySelector("textarea");
+      addressField.addEventListener("input", () => {
+        const bagCheckbox = document.querySelector("#ill-bag-checkbox");
+        const boxCheckbox = document.querySelector("#ill-box-checkbox");
+        addressField.value.includes("**BAG**")
+          ? (bagCheckbox.checked = true)
+          : (bagCheckbox.checked = false);
+        addressField.value.includes("**RETURN IN BOX**")
+          ? (boxCheckbox.checked = true)
+          : (boxCheckbox.checked = false);
+      });
+    };
+  }
+
+  createILLPageMods();
+})();
