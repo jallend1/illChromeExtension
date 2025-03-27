e0b14a18cabd01ef7d4bfd0499f6913ee340ae65
Jason Allen <39307371+jallend1@users.noreply.github.com>
2025-03-27 14:13:53 -0700

    Modifies dismissOpenTransit into a self-executing function to resolve scope variable declarations popping up in the console

--- scripts/dismissOpenTransit.js
+++ scripts/dismissOpenTransit.js
@@ -1,31 +1,30 @@
-function dismissOpenTransit() {
-  const modal = document.querySelector(".modal-body");
+(() => {
+  function dismissOpenTransit() {
+    const modal = document.querySelector(".modal-body");
 
-  if (!modal) {
-    return;
+    if (!modal) {
+      return;
+    }
+
+    const modalText = modal.textContent;
+
+    if (modalText.includes("open transit on item")) {
+      const modalFooterButton = document.querySelector(".modal-footer button");
+      if (modalFooterButton) modalFooterButton.click();
+    }
   }
 
-  const modalText = modal.textContent;
+  const observer = new MutationObserver(() => {
+    dismissOpenTransit();
+  });
 
-  if (modalText.includes("open transit on item")) {
-    const modalFooterButton = document.querySelector(".modal-footer button");
-    if (modalFooterButton) modalFooterButton.click();
-  }
-}
+  observer.observe(document.body, {
+    childList: true,
+    subtree: true,
+  });
 
-// dismissOpenTransit();
-
-const observer = new MutationObserver(() => {
-  console.log("Mutation observed!!!!!!");
-  dismissOpenTransit();
-});
-
-observer.observe(document.body, {
-  childList: true,
-  subtree: true,
-});
-
-window.addEventListener("beforeunload", () => {
-  clearInterval(intervalID);
-  observer.disconnect();
-});
+  window.addEventListener("beforeunload", () => {
+    clearInterval(intervalID);
+    observer.disconnect();
+  });
+})();
