d319a26c44f7b33a10e54cdfada049e5621abaf6
Jason Allen <39307371+jallend1@users.noreply.github.com>
2025-03-28 15:37:02 -0700

    Commented out an approach I'm abandoning because it feels like madness

--- scripts/dismissOpenTransit.js
+++ scripts/dismissOpenTransit.js
@@ -15,44 +15,55 @@
     }
   }
 
-  const observer = new MutationObserver(() => {
+  const observer = new MutationObserver((mutations) => {
+    console.log(mutations);
     dismissOpenTransit();
     monitorInput();
   });
 
-  function monitorInput() {
-    const barcodeInput = document.querySelector("#barcode-input");
-    if (barcodeInput && !barcodeInput.dataset.listenerAdded) {
-      barcodeInput.addEventListener("keydown", (e) => {
-        const holdsCountText =
-          document.querySelector("#ngb-nav-2")?.textContent;
-        console.log(holdsCountText);
-        if (holdsCountText) {
-          const match = holdsCountText.match(/\(\d+\s*\/\s*(\d+)\)/);
-          if (match) {
-            const secondNumber = match[1];
+  // function monitorInput() {
+  //   const barcodeInput = document.querySelector("#barcode-input");
+  //   if (barcodeInput && !barcodeInput.dataset.listenerAdded) {
+  //     barcodeInput.addEventListener("keydown", (e) => {
+  //       const holdsCountText =
+  //         document.querySelector("#ngb-nav-2")?.textContent;
+  //       console.log(holdsCountText);
+  //       if (holdsCountText) {
+  //         const match = holdsCountText.match(/\(\d+\s*\/\s*(\d+)\)/);
+  //         if (match) {
+  //           const secondNumber = match[1];
 
-            barcodeInput.dataset.totalHolds = secondNumber;
-            console.log("Total holds should be:", secondNumber);
-          }
-        }
-        if (e.key === "Enter") {
-          console.log("Enter is pressed");
-        }
-      });
-      barcodeInput.dataset.listenerAdded = true;
-    }
-  }
+  //           barcodeInput.dataset.totalHolds = secondNumber;
+  //           console.log("Total holds should be:", secondNumber);
+  //         }
+  //       }
+  //       if (e.key === "Enter") {
+  //         console.log("Enter is pressed");
+  //       }
+  //     });
+  //     barcodeInput.dataset.listenerAdded = true;
+  //   }
+  // }
 
   // TODO: What am I doing.
-  // 1) Keep track of the number of holds in the holds field #ngb-nav-2.textContent
-  // 2) Track mutations on the list of currently checked out items -- New children in childlist of .eg-grid-body
-  // 3) When the list of currently checked out items goes up, check if the number of holds went down by 1
-  // 4) If so, then we know that the user checked out an item with a hold on it (Success!)
-  // 5) If it didn't go down, populate an error modal indicating so
+  // 1) Observe mutations Items Out and Holds fields
+  const holdsField = document.querySelector("[ngbnavitem='holds'] > a");
+  const itemsOutField = document.querySelector("[ngbnavitem='items_out'] > a");
+  console.log(holdsField.textContent);
+  console.log(itemsOutField.textContent);
 
-  const barcodeInput = document.querySelector("#barcode-input");
-  console.log(barcodeInput);
+  observer.observer(holdsField, {
+    characterData: true,
+  });
+
+  observer.observe(itemsOutField, {
+    characterData: true,
+  });
+
+  // 2) When Items Out goes up, check if the number of holds went down by 1
+  // 3) If not, throw an error modal indicating as much
+
+  // const barcodeInput = document.querySelector("#barcode-input");
 
   observer.observe(document.body, {
     childList: true,
