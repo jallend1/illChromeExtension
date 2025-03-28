ec986d5be74acd5b1a8a42bc1f7431e8d2af2928
Jason Allen <39307371+jallend1@users.noreply.github.com>
2025-03-28 14:04:08 -0700

    Begins implementation of check to ensure number of total holds goes down on checkout of item

--- scripts/dismissOpenTransit.js
+++ scripts/dismissOpenTransit.js
@@ -16,7 +16,32 @@
 
   const observer = new MutationObserver(() => {
     dismissOpenTransit();
+    monitorInput();
   });
+
+  function monitorInput() {
+    const barcodeInput = document.querySelector("#barcode-input");
+    if (barcodeInput && !barcodeInput.dataset.listenerAdded) {
+      barcodeInput.addEventListener("keydown", (e) => {
+        const holdsCountText =
+          document.querySelector("#ngb-nav-2")?.textContent;
+        console.log(holdsCountText);
+        if (holdsCountText) {
+          const match = holdsCountText.match(/\(\d+\s*\/\s*(\d+)\)/);
+          if (match) {
+            const secondNumber = match[1];
+
+            barcodeInput.dataset.totalHolds = secondNumber;
+            console.log("Total holds should be:", secondNumber);
+          }
+        }
+        if (e.key === "Enter") {
+          console.log("Enter is pressed");
+        }
+      });
+      barcodeInput.dataset.listenerAdded = true;
+    }
+  }
 
   observer.observe(document.body, {
     childList: true,
