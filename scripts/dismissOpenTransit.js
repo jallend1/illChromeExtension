2617dc6a3148445b6c7818b71e471cd778fc1027
Jason Allen <39307371+jallend1@users.noreply.github.com>
2025-03-29 12:12:44 -0700

    Establishes rules for ignoring Evergreen's intermediate drop to zero on items out

--- scripts/dismissOpenTransit.js
+++ scripts/dismissOpenTransit.js
@@ -18,64 +18,89 @@
   const observer = new MutationObserver((mutations) => {
     // console.log(mutations);
     mutations.forEach((mutation) => {
-      if (mutation.type === "characterData") {
+      if (mutation.type === "characterData" && listeningForBarcode) {
+        console.dir(mutation);
+        if (mutation.oldValue.includes(" Items Out")) {
+          const oldValue = mutation.oldValue.match(/(?<=\()\d+(?=\))/)[0]; // Extracts the number in parentheses in nav field
+          const currentText = mutation.target.textContent.trim();
+          const currentValue = currentText.match(/(?<=\()\d+(?=\))/)[0]; // Extracts the number in parentheses in nav field
+
+          // On update, Evergreen briefly sets items out to zero regardless of
+          // how many items are actually out before populating it with the new value
+          // Probably just ignore this period?
+          if (currentValue < oldValue) {
+            return;
+          }
+          if (currentValue > oldValue) {
+            console.log("Done resetting, probably");
+            console.log("Current value:", currentValue);
+            console.log("Old value:", oldValue);
+          }
+        }
+      } else if (mutation.type === "characterData") {
         console.log("Character data changed:", mutation.target.textContent);
+        console.log("old value:", mutation.oldValue);
       }
     });
     dismissOpenTransit();
     // monitorInput();
   });
 
-  // function monitorInput() {
-  //   const barcodeInput = document.querySelector("#barcode-input");
-  //   if (barcodeInput && !barcodeInput.dataset.listenerAdded) {
-  //     barcodeInput.addEventListener("keydown", (e) => {
-  //       const holdsCountText =
-  //         document.querySelector("#ngb-nav-2")?.textContent;
-  //       console.log(holdsCountText);
-  //       if (holdsCountText) {
-  //         const match = holdsCountText.match(/\(\d+\s*\/\s*(\d+)\)/);
-  //         if (match) {
-  //           const secondNumber = match[1];
+  let listeningForBarcode = false;
 
-  //           barcodeInput.dataset.totalHolds = secondNumber;
-  //           console.log("Total holds should be:", secondNumber);
-  //         }
-  //       }
-  //       if (e.key === "Enter") {
-  //         console.log("Enter is pressed");
-  //       }
-  //     });
-  //     barcodeInput.dataset.listenerAdded = true;
-  //   }
-  // }
+  function monitorInput() {
+    const barcodeInput = document.querySelector("#barcode-input");
+    if (barcodeInput && !barcodeInput.dataset.listenerAdded) {
+      barcodeInput.addEventListener("keydown", (e) => {
+        // const holdsCountText =
+        //   document.querySelector("#ngb-nav-2")?.textContent;
+        // console.log(holdsCountText);
+        // if (holdsCountText) {
+        //   const match = holdsCountText.match(/\(\d+\s*\/\s*(\d+)\)/);
+        //   if (match) {
+        //     const secondNumber = match[1];
 
-  // TODO: What am I doing.
+        //     barcodeInput.dataset.totalHolds = secondNumber;
+        //     console.log("Total holds should be:", secondNumber);
+        //   }
+        // }
+        if (e.key === "Enter") {
+          console.log("Enter is pressed");
+          listeningForBarcode = true;
+        }
+      });
+      barcodeInput.dataset.listenerAdded = true;
+    }
+  }
+
+  // TODO: What in the world am I doing.
   // 1) Observe mutations Items Out and Holds fields
-  const holdsField = document.querySelector(
-    "[ngbnavitem='holds'] > a"
-  )?.firstChild; // Targets text node inside the anchor tag
-  const itemsOutField = document.querySelector(
-    "[ngbnavitem='items_out'] > a"
-  )?.firstChild; // Targets text node inside the anchor tag
+  const holdsField = document.querySelector("[ngbnavitem='holds'] > a");
+  const itemsOutField = document.querySelector("[ngbnavitem='items_out'] > a");
   if (holdsField) console.log(holdsField.textContent);
   if (itemsOutField) console.log(itemsOutField.textContent);
 
-  if (holdsField) {
-    observer.observe(holdsField, {
+  if (holdsField && !holdsField.dataset.observerAdded) {
+    // Adds observer to the text node (first child)
+    observer.observe(holdsField.firstChild, {
       characterData: true,
+      characterDataOldValue: true,
     });
+    holdsField.dataset.observerAdded = true; // Adds the observer just the one time
   }
-  if (itemsOutField) {
-    observer.observe(itemsOutField, {
+  if (itemsOutField && !itemsOutField.dataset.observerAdded) {
+    // Adds observer to the text node (first child)
+    observer.observe(itemsOutField.firstChild, {
       characterData: true,
+      characterDataOldValue: true,
     });
+    itemsOutField.dataset.observerAdded = true; // Adds the observer just the one time
   }
 
-  // 2) When Items Out goes up, check if the number of holds went down by 1
+  // 2 ) If enter is pressed, check if the number of holds went down by 1
+  monitorInput();
+
   // 3) If not, throw an error modal indicating as much
-
-  // const barcodeInput = document.querySelector("#barcode-input");
 
   observer.observe(document.body, {
     childList: true,
