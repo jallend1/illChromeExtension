ee70a11a3a9ddcbca05a2ac7f4423300b7cfdf48
Jason Allen <39307371+jallend1@users.noreply.github.com>
2025-04-13 10:39:23 -0700

    Expands retrievePatron to also navigate to billing tab and open the 'Add Billing' modal

--- scripts/retrievePatron.js
+++ scripts/retrievePatron.js
@@ -1,18 +1,19 @@
 (async () => {
-  console.log("Running retrievePatron.js...");
-
-  const waitForElementWithInterval = (selector) =>
+  const waitForElementWithInterval = (selectorOrFunction) =>
     new Promise((resolve, reject) => {
       const startTime = Date.now();
-
       const intervalId = setInterval(() => {
-        const element = document.querySelector(selector);
+        const element =
+          typeof selectorOrFunction === "function"
+            ? selectorOrFunction() // If a function, call it to get the element
+            : document.querySelector(selectorOrFunction); // If a selector, go to town
+
         if (element) {
-          clearInterval(intervalId); // Clears interval when we got the element
+          clearInterval(intervalId); // Clears interval when element is found
           resolve(element);
         } else if (Date.now() - startTime > 10000) {
           clearInterval(intervalId);
-          reject(new Error(`Element with selector "${selector}" not found.`));
+          reject(new Error(`Element not found.`));
         }
       }, 100);
     });
@@ -26,17 +27,45 @@
     );
 
     chrome.storage.local.get("patronBarcode", (result) => {
-      console.log(result.patronBarcode);
       if (!result.patronBarcode)
         throw new Error("Patron ID not found in storage.");
-
-      console.log(result.patronBarcode);
       inputField.value = result.patronBarcode;
       const event = new Event("input", { bubbles: true, cancelable: true });
       inputField.dispatchEvent(event);
       submitButton.click();
+      goToBilling();
+      clickAddBilling();
+      chrome.storage.local.remove("patronBarcode");
     });
   } catch (error) {
     console.error(error.message);
   }
+
+  const goToBilling = async () => {
+    // Waits for link to billing tab to appear on patron page
+    try {
+      const billingLink = await waitForElementWithInterval(() => {
+        return Array.from(document.querySelectorAll("a")).find((link) =>
+          link.textContent.includes("Bills")
+        );
+      });
+      billingLink.click();
+    } catch (error) {
+      console.error("Billing link not found:", error.message);
+    }
+  };
+
+  const clickAddBilling = async () => {
+    // Waits for button to add billing info to appear on billing tab page
+    try {
+      const addBillingButton = await waitForElementWithInterval(() => {
+        return Array.from(document.querySelectorAll("button")).find((button) =>
+          button.textContent.includes("Add Billing")
+        );
+      });
+      addBillingButton.click();
+    } catch (error) {
+      console.error("Add Billing button not found:", error.message);
+    }
+  };
 })();
