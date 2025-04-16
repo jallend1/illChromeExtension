c1c57948a683d8f67a75cf32de70033162ca8a34
Jason Allen <39307371+jallend1@users.noreply.github.com>
2025-04-16 14:51:10 -0700

    Begins implementation of WorldShare style changes

---
+++ scripts/worldShareMods.js
@@ -0,0 +1,50 @@
+// TODO: Insert this with background.js on appropriate pages
+(async () =>{
+    // Same function as in retrievePatron.js
+  const waitForElementWithInterval = (selectorOrFunction) =>
+    new Promise((resolve, reject) => {
+      const startTime = Date.now();
+      const intervalId = setInterval(() => {
+        const element =
+          typeof selectorOrFunction === "function"
+            ? selectorOrFunction() // If a function, call it to get the element
+            : document.querySelector(selectorOrFunction); // If a selector, go to town
+
+        if (element) {
+          clearInterval(intervalId); // Clears interval when element is found
+          resolve(element);
+        } else if (Date.now() - startTime > 10000) {
+          clearInterval(intervalId);
+          reject(new Error(`Element not found.`));
+        }
+      }, 100);
+    });
+
+    const applyStyles = (el) => {
+        el.style.backgroundColor = "red";
+        el.style.color = "white";
+        el.style.padding = "0.4rem";
+        el.style.borderRadius = "0.5rem";
+        el.style.fontWeight = "bold";
+    }
+
+    // Select span element with data attribute of "requestStatus"
+    const requestStatus = waitForElementWithInterval("[data='requestStatus']");
+    if(!requestStatus) {
+      console.error("Request status not found.");
+      return;
+    }
+
+    // If the request status is recalled, emphasize it
+    if(requestStatus.innerText.includes("Recalled")) applyStyles(requestStatus);
+    else if(requestStatus.innerText.includes("Received")) {
+        const dispositionElement = document.querySelector("[data='disposition']");
+        if(dispositionElement && dispositionElement.innerText.includes("Overdue")) {
+            applyStyles(dispositionElement);
+            applyStyles(requestStatus);
+        }
+    }
+    
+
+
+})()
