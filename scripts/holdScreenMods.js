d9588b21a267bc7877f76c34fdc8b07ef84b6647
Jason Allen <39307371+jallend1@users.noreply.github.com>
2025-04-22 15:52:21 -0700

    Cleans up debugging code and begins implentation of Edit Selected Patron button

--- scripts/holdScreenMods.js
+++ scripts/holdScreenMods.js
@@ -155,46 +155,49 @@
   // * unable to get patron ID through *
   // * barcode at this time            *
   // ***********************************
-  // const createEditPatronButton = () => {
-  // Ugly, but most reliable selector for the search button
-  //   const searchButton = document.querySelector(
-  //     "button > span.align-middle"
-  //   ).parentElement;
-  //   const searchButtonDiv = searchButton.parentElement;
+  const createEditPatronButton = () => {
+    // Ugly, but most reliable selector for the search button
+    const searchButton = document.querySelector(
+      "button > span.align-middle"
+    ).parentElement;
+    const searchButtonDiv = searchButton.parentElement;
 
-  //   const createButton = () => {
-  //     const newButton = document.createElement("button");
-  //     newButton.type = "button";
-  //     newButton.classList.add("btn", "btn-outline-dark", "btn-sm");
-  //     newButton.style.height = "100%";
-  //     const newSpan = createSpanText();
-  //     newButton.appendChild(newSpan);
-  //     newButton.addEventListener("click", () => {
-  //       const currentPatron = document.querySelector("#patron-barcode").value;
-  //       console.log(currentPatron);
-  //     });
-  //     return newButton;
-  //   };
+    const createButton = () => {
+      const newButton = document.createElement("button");
+      newButton.type = "button";
+      newButton.classList.add("btn", "btn-outline-dark", "btn-sm");
+      newButton.style.height = "100%";
+      const newSpan = createSpanText();
+      newButton.appendChild(newSpan);
+      newButton.addEventListener("click", () => {
+        const currentPatron = document.querySelector("#patron-barcode").value;
+        console.log(currentPatron);
+      });
+      newButton.disabled = true;
+      return newButton;
+    };
 
-  //   const createContainerDiv = () => {
-  //     const containerDiv = document.createElement("div");
-  //     containerDiv.appendChild(createButton());
-  //     containerDiv.classList.add("col-lg-2");
-  //     return containerDiv;
-  //   };
+    const createContainerDiv = () => {
+      const containerDiv = document.createElement("div");
+      containerDiv.appendChild(createButton());
+      containerDiv.classList.add("col-lg-2");
+      return containerDiv;
+    };
 
-  //   const createSpanText = () => {
-  //     const newSpan = document.createElement("span");
-  //     newSpan.classList.add("align-middle");
-  //     newSpan.textContent = "Edit Selected Patron";
-  //     return newSpan;
-  //   };
+    const createSpanText = () => {
+      const newSpan = document.createElement("span");
+      newSpan.classList.add("align-middle");
+      newSpan.textContent = "Edit Selected Patron";
+      return newSpan;
+    };
 
-  //   searchButtonDiv.parentElement.appendChild(createContainerDiv());
-  // }
+    searchButtonDiv.parentElement.appendChild(createContainerDiv());
+  };
 
   // function sendMessageToBackground() {
   //   const urlSuffix = `search?org=1&limit=10&query=${searchQuery}%20&fieldClass=keyword&joinOp=&matchOp=contains&dateOp=is&ridx=122`;
   //   chrome.runtime.sendMessage({ action: "holdScreenMods", url: urlSuffix });
   // }
+
+  createEditPatronButton();
 })();
