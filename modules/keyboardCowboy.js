bc1cfc9ed04577ee87aea223986e0386bc2b3175
Jason Allen <39307371+jallend1@users.noreply.github.com>
2025-05-08 12:45:19 -0700

    Fixes spacing issue on keyboard cowboy and refactors for clarity

--- modules/keyboardCowboy.js
+++ modules/keyboardCowboy.js
@@ -1,57 +1,29 @@
 export const keyboardCowboy = (message) => {
   const mainSection = document.querySelector("main");
+  const existing = document.getElementById("keyboard-cowboy-tooltip");
+  const cactusCowboy = chrome.runtime.getURL("images/cowboy.png");
+  if (existing) existing.remove();
 
-  const removeTooltip = () => {
-    const tooltip = document.querySelector("#keyboard-cowboy-tooltip");
-    tooltip.remove();
-  };
+  const tooltip = document.createElement("div");
+  tooltip.id = "keyboard-cowboy-tooltip";
+  Object.assign(tooltip.style, {
+    margin: "35px 1rem 0 auto",
+    padding: "1rem",
+    textAlign: "center",
+    width: "300px",
+    backgroundColor: "#fff3cd",
+    border: "1px solid #e9ecef",
+    borderRadius: "0.25rem",
+    zIndex: "1000",
+  });
 
-  const checkExistingTooltip = () => {
-    const existingTooltip = document.querySelector("#keyboard-cowboy-tooltip");
-    if (existingTooltip) {
-      removeTooltip();
-    }
-  };
+  tooltip.innerHTML = `
+    <header><h2>Be a keyboard cowboy!</h2></header>
+    <main style="display: flex; flex-direction: row;">
+        <img src="${cactusCowboy}" style="height:100px;width:100px;margin-right:10px;">
+      <p style="color:#000; margin:0;">${message}</p>
+    </main>
+  `;
 
-  const addTooltip = () => {
-    const tooltip = document.createElement("div");
-    tooltip.id = "keyboard-cowboy-tooltip";
-    tooltip.style.margin = "1rem 1rem 0 auto";
-    tooltip.style.padding = "1rem";
-
-    tooltip.style.textAlign = "center";
-    tooltip.style.width = "300px";
-    tooltip.style.backgroundColor = "#fff3cd";
-    tooltip.style.border = "1px solid #e9ecef";
-    tooltip.style.borderRadius = "0.25rem";
-    tooltip.style.zIndex = "1000";
-
-    const header = document.createElement("header");
-    const headerText = document.createElement("h2");
-    headerText.textContent = "Be a keyboard cowboy!";
-    header.appendChild(headerText);
-
-    const main = document.createElement("main");
-    main.style.display = "flex";
-    main.style.flexDirection = "row";
-
-    const img = document.createElement("img");
-    img.src = chrome.runtime.getURL("images/cowboy.png");
-    img.style.height = "100px";
-    img.style.width = "100px";
-    img.style.marginRight = "10px";
-
-    const p = document.createElement("p");
-    p.style.color = "#000";
-    p.innerHTML = message;
-    main.appendChild(img);
-    main.appendChild(p);
-
-    tooltip.appendChild(header);
-    tooltip.appendChild(main);
-
-    mainSection?.parentElement?.insertBefore(tooltip, mainSection);
-  };
-  checkExistingTooltip();
-  addTooltip();
+  mainSection?.parentElement?.insertBefore(tooltip, mainSection);
 };
