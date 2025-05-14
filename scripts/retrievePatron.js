394ee0d0e21200412983ddffb92b28131d4aff07
Jason Allen <39307371+jallend1@users.noreply.github.com>
2025-05-14 09:06:13 -0700

    Moves adding lending fee logic to isolated file

--- scripts/retrievePatron.js
+++ scripts/retrievePatron.js
@@ -2,6 +2,9 @@
   const { waitForElementWithInterval } = await import(
     chrome.runtime.getURL("modules/utils.js")
   );
+  const { addLendingFee } = await import(
+    chrome.runtime.getURL("modules/addLendingFee.js")
+  );
 
   try {
     const inputField = await waitForElementWithInterval(
@@ -29,11 +32,9 @@
         inputField.dispatchEvent(event);
         submitButton.click();
         // TODO: Provide some clarity to the user that the fee is being added
-        goToBillingTab();
-        clickAddBilling();
-        addBillingNotes(title, fee);
-
+        addLendingFee(title, fee);
         chrome.storage.local.remove("request");
+        return;
       } else {
         console.error("No valid data found in local storage.");
       }
@@ -41,53 +42,5 @@
   } catch (error) {
     console.error(error.message);
   }
-
-  const goToBillingTab = async () => {
-    // Waits for link to billing tab to appear on patron page
-    try {
-      const billingLink = await waitForElementWithInterval(() => {
-        return Array.from(document.querySelectorAll("a")).find((link) =>
-          link.textContent.includes("Bills")
-        );
-      });
-      billingLink.click();
-    } catch (error) {
-      console.error("Billing link not found:", error.message);
-    }
-  };
-
-  const clickAddBilling = async () => {
-    // Waits for button to add billing info to appear on billing tab page
-    try {
-      const addBillingButton = await waitForElementWithInterval(() => {
-        return Array.from(document.querySelectorAll("button")).find((button) =>
-          button.textContent.includes("Add Billing")
-        );
-      });
-      addBillingButton.click();
-    } catch (error) {
-      console.error("Add Billing button not found:", error.message);
-    }
-  };
-
-  const addBillingNotes = async (title, fee) => {
-    // Waits for billing modal to appear and then fills out fee and title
-    try {
-      const billingModal = await waitForElementWithInterval(() => {
-        return document.querySelector(".modal-content");
-      });
-      if (!billingModal) throw new Error("Billing modal not found.");
-
-      const feeInput = billingModal.querySelector("#amount-input");
-      const titleInput = billingModal.querySelector("textarea");
-
-      feeInput.value = fee;
-      titleInput.value = `Patron approved lending fee for "ILL Title - ${title}" sent out from ILL today.`;
-
-      const submitButton = billingModal.querySelector(".btn.btn-success");
-      submitButton.focus();
-    } catch (error) {
-      console.error("Billing modal not found:", error.message);
-    }
-  };
+ 
 })();
