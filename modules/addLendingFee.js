95cf0f50092dc986e924be515ff497b1547d9072
Jason Allen <39307371+jallend1@users.noreply.github.com>
2025-05-14 09:05:27 -0700

    Extracts addLendingFee logic to isolated file

---
+++ modules/addLendingFee.js
@@ -0,0 +1,59 @@
+ const { waitForElementWithInterval } = await import(
+        chrome.runtime.getURL("modules/utils.js")
+        );
+
+    const addBillingNotes = async (title, fee) => {
+    // Waits for billing modal to appear and then fills out fee and title
+    try {
+      const billingModal = await waitForElementWithInterval(() => {
+        return document.querySelector(".modal-content");
+      });
+      if (!billingModal) throw new Error("Billing modal not found.");
+
+      const feeInput = billingModal.querySelector("#amount-input");
+      const titleInput = billingModal.querySelector("textarea");
+
+      feeInput.value = fee;
+      titleInput.value = `Patron approved lending fee for "ILL Title - ${title}" sent out from ILL today.`;
+
+      const submitButton = billingModal.querySelector(".btn.btn-success");
+      submitButton.focus();
+    } catch (error) {
+      console.error("Billing modal not found:", error.message);
+    }
+  };   
+  
+   const goToBillingTab = async () => {
+    // Waits for link to billing tab to appear on patron page
+    try {
+        const billingLink = await waitForElementWithInterval(() => {
+        return Array.from(document.querySelectorAll("a"))
+            .find((link) => link.textContent.includes("Bills")
+        );
+      });
+      billingLink.click();
+    } catch (error) {
+      console.error("Billing link not found:", error.message);
+    }
+  };
+
+      const clickAddBilling = async () => {
+    // Waits for button to add billing info to appear on billing tab page
+    try {
+      const addBillingButton = await waitForElementWithInterval(() => {
+        return Array.from(document.querySelectorAll("button")).find((button) =>
+          button.textContent.includes("Add Billing")
+        );
+      });
+      addBillingButton.click();
+    } catch (error) {
+      console.error("Add Billing button not found:", error.message);
+    }
+  };
+
+export const addLendingFee = async (title, fee) => {
+    
+goToBillingTab();
+    clickAddBilling();
+    addBillingNotes(title, fee);
+  }
