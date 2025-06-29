5d1f0edfc4cc850124b59abba26530b045c26bda
Jason Allen <39307371+jallend1@users.noreply.github.com>
2025-06-29 08:29:55 -0700

    Adds functionality to paste patron info into WorldShare

--- scripts/copyPatronFromRM.js
+++ scripts/copyPatronFromRM.js
@@ -1,13 +1,4 @@
 (async () => {
-  //   const titleField = document.querySelector(
-  //     "body > ngb-modal-window > div > div > div.modal-body.form-validated > div:nth-child(2) > div.col-10 > input"
-  //   );
-  //   const authorField = document.querySelector(
-  //     "body > ngb-modal-window > div > div > div.modal-body.form-validated > div:nth-child(4) > div:nth-child(4) > input"
-  //   );
-  //   const title = titleField ? titleField.value.trim() : "";
-  //   const author = authorField ? authorField.value.trim() : "";
-  //   const clipboardContent = `${title} ${author}`;
   const patronName = document
     .querySelector("div.modal-body.form-validated > div")
     .children[1].textContent.split("(")[0]
@@ -37,12 +28,4 @@
       //   console.log(requestManagerPatron);
     }
   );
-  //   navigator.clipboard
-  //     .writeText(clipboardContent)
-  //     .then(() => {
-  //       console.log("Title and Author copied to clipboard:", clipboardContent);
-  //     })
-  //     .catch((err) => {
-  //       console.error("Failed to copy Title and Author:", err);
-  //     });
 })();
---
+++ scripts/pastePatronToWS.js
@@ -0,0 +1,33 @@
+(async () => {
+  const { waitForElementWithInterval } = await import(
+    chrome.runtime.getURL("modules/utils.js")
+  );
+
+  const nameSelector = "#patron-details-NEW-REQUESTER-patronName";
+  const barcodeSelector = "#patron-details-NEW-REQUESTER-patronId";
+
+  let patronName;
+  let barcode;
+  // Extract requestManagerPatron from local storage
+  chrome.storage.local.get("requestManagerPatron", (data) => {
+    if (data.requestManagerPatron) {
+      patronName = data.requestManagerPatron.name;
+      barcode = data.requestManagerPatron.barcode;
+      console.log("Request Manager Patron loaded from storage:", {
+        name: patronName,
+        barcode: barcode,
+      });
+    }
+  });
+
+  // Wait for the name and barcode fields to be available
+  const nameField = await waitForElementWithInterval(nameSelector);
+  const barcodeField = await waitForElementWithInterval(barcodeSelector);
+
+  // Scroll to nameSelector field
+  nameField.scrollIntoView({ behavior: "smooth", block: "center" });
+
+  // Fill in the name and barcode fields
+  nameField.value = patronName;
+  barcodeField.value = barcode;
+})();
