90f26820e8b295197bfcb3ca7606596cb9edf767
Jason Allen <39307371+jallend1@users.noreply.github.com>
2025-07-03 12:33:03 -0700

    Removes pastePatronToWS after integrating it into copyPatron

--- scripts/copyPatronFromRM.js
+++ scripts/copyPatronFromRM.js
@@ -1,4 +1,3 @@
-// TODO: Remove pastePatronToWS.js entirely
 // TODO: Rename this script to reflect consolidated functionality
 
 (async () => {
@@ -9,10 +8,13 @@
   const extractPatronDetails = async () => {
     const requestManagerSelectors = {
       patronNameField: "div.modal-body.form-validated > div",
-      pickupLocationField: "body > ngb-modal-window > div > div > div.modal-body.form-validated > div:nth-child(8) > div:nth-child(2)",
-    }
+      pickupLocationField:
+        "body > ngb-modal-window > div > div > div.modal-body.form-validated > div:nth-child(8) > div:nth-child(2)",
+    };
 
-    const patronNameField = document.querySelector(requestManagerSelectors.patronNameField).children[1].textContent.trim();
+    const patronNameField = document
+      .querySelector(requestManagerSelectors.patronNameField)
+      .children[1].textContent.trim();
 
     const patronName = patronNameField.split(" (")[0].trim();
     const barcode = patronNameField.match(/\((\d+)\)/)[1];
@@ -23,7 +25,7 @@
       .split(")")[0]
       .trim();
     const worldShareName = patronName + ", " + pickupLocation;
-    
+
     // Stores name and barcode in local storage
     chrome.storage.local.set(
       { requestManagerPatron: { name: worldShareName, barcode } },
@@ -33,65 +35,66 @@
         pastePatronToWS();
       }
     );
-}
+  };
 
-const pastePatronToWS = async () => {
-  const nameSelector = "#patron-details-NEW-REQUESTER-patronName";
-  const barcodeSelector = "#patron-details-NEW-REQUESTER-patronId";
+  const pastePatronToWS = async () => {
+    const nameSelector = "#patron-details-NEW-REQUESTER-patronName";
+    const barcodeSelector = "#patron-details-NEW-REQUESTER-patronId";
 
-  // Check if we're on the right page
-  if (!window.location.href.includes("share.worldcat.org")) {
-    console.log(
-      "Not on WorldShare page, finding and switching to WorldShare tab..."
-    );
-    chrome.runtime.sendMessage({
-      type: "findAndSwitchToWorldShare",
-      scriptToRelaunch: "copyPatronFromRM",
+    // Check if we're on the right page
+    if (!window.location.href.includes("share.worldcat.org")) {
+      console.log(
+        "Not on WorldShare page, finding and switching to WorldShare tab..."
+      );
+      chrome.runtime.sendMessage({
+        type: "findAndSwitchToWorldShare",
+        scriptToRelaunch: "copyPatronFromRM",
+      });
+      return; // Don't wait for a response!
+    }
+
+    const storageData = await new Promise((resolve) => {
+      chrome.storage.local.get("requestManagerPatron", resolve);
     });
-    return; // Don't wait for a response!
+
+    if (!storageData.requestManagerPatron) {
+      console.error("No patron data found in storage");
+      return;
+    }
+
+    const { name: patronName, barcode } = storageData.requestManagerPatron;
+    console.log("Request Manager Patron loaded from storage:", {
+      name: patronName,
+      barcode: barcode,
+    });
+
+    // Wait for the name and barcode fields to be available
+    const nameField = await waitForElementWithInterval(nameSelector);
+    const barcodeField = await waitForElementWithInterval(barcodeSelector);
+
+    if (!nameField || !barcodeField) {
+      console.error("Could not find required fields on WorldShare page");
+      return;
+    }
+
+    // Scroll to nameSelector field
+    nameField.scrollIntoView({ behavior: "smooth", block: "center" });
+
+    // Fill in the name and barcode fields
+    nameField.value = patronName;
+    barcodeField.value = barcode;
+  };
+
+  // If the script is running in the Request Manager modal, extract patron details
+  if (window.location.href.includes("eg2/en-US/staff/cat/requests")) {
+    extractPatronDetails();
   }
-
-  const storageData = await new Promise((resolve) => {
-    chrome.storage.local.get("requestManagerPatron", resolve);
-  });
-
-  if (!storageData.requestManagerPatron) {
-    console.error("No patron data found in storage");
-    return;
+  // If the script is running in WorldShare, paste patron details
+  else if (window.location.href.includes("share.worldcat.org")) {
+    pastePatronToWS();
+  } else {
+    console.error(
+      "This script should only run in Request Manager or WorldShare pages."
+    );
   }
-
-  const { name: patronName, barcode } = storageData.requestManagerPatron;
-  console.log("Request Manager Patron loaded from storage:", {
-    name: patronName,
-    barcode: barcode,
-  });
-
-  // Wait for the name and barcode fields to be available
-  const nameField = await waitForElementWithInterval(nameSelector);
-  const barcodeField = await waitForElementWithInterval(barcodeSelector);
-
-  if (!nameField || !barcodeField) {
-    console.error("Could not find required fields on WorldShare page");
-    return;
-  }
-
-  // Scroll to nameSelector field
-  nameField.scrollIntoView({ behavior: "smooth", block: "center" });
-
-  // Fill in the name and barcode fields
-  nameField.value = patronName;
-  barcodeField.value = barcode;
-};
-
-// If the script is running in the Request Manager modal, extract patron details
-if(window.location.href.includes("eg2/en-US/staff/cat/requests")) {
-extractPatronDetails();
-}
-// If the script is running in WorldShare, paste patron details
-else if (window.location.href.includes("share.worldcat.org")) {
-  pastePatronToWS();
-} else {
-  console.error("This script should only run in Request Manager or WorldShare pages.");
-}
-
 })();
