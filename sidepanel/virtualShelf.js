74729aaa4ef4793140060f1096273b3cbedfc386
Jason Allen <39307371+jallend1@users.noreply.github.com>
2025-07-08 10:16:36 -0700

    Groups locations by state

--- sidepanel/virtualShelf.js
+++ sidepanel/virtualShelf.js
@@ -24,43 +24,48 @@
     const existingStateElement = document.getElementById(
       libraryState.replace(/\s+/g, "-").toLowerCase()
     );
-    if (existingStateElement) {
+    // TODO: Add logic for this bad boy
+    if (!existingStateElement) {
       console.log(
         `State ${libraryState} already exists! Find a way to add this book to it.`
       );
+
+      const stateElement = document.createElement("div");
+      stateElement.className = "state-container";
+      stateElement.id = libraryState.replace(/\s+/g, "-").toLowerCase();
+      const headingState = document.createElement("h2");
+      headingState.className = "state";
+      headingState.id = libraryState.replace(/\s+/g, "-").toLowerCase();
+      headingState.textContent = `${libraryState}`;
+      stateElement.appendChild(headingState);
+      virtualShelfContainer.appendChild(stateElement);
     }
 
     // Create elements
     const bookElement = document.createElement("div");
     bookElement.className = "book";
-    const headingState = document.createElement("h2");
-    headingState.className = "state";
-    headingState.id = libraryState.replace(/\s+/g, "-").toLowerCase();
-    headingState.textContent = `${libraryState}`;
+
     const dueDateElement = document.createElement("p");
     dueDateElement.className = "due-date";
+
     const libraryNameElement = document.createElement("h3");
     libraryNameElement.className = "library-name";
+    libraryNameElement.textContent = `${libraryName}`;
 
     // Create individual spans for due date and title
     const dueDateSpan = document.createElement("span");
     dueDateSpan.className = "due-date-span";
-    dueDateSpan.textContent = dueDate.toLocaleDateString();
+    dueDateSpan.textContent = `${dueDate.toLocaleDateString()} - `;
 
     const titleSpan = document.createElement("span");
     titleSpan.className = "title-span";
     titleSpan.textContent = title;
 
-    // Set text content
-    headingState.textContent = `${libraryState}`;
-    libraryNameElement.textContent = `${libraryName}`;
-
     // Append spans to the due date element
     dueDateElement.appendChild(dueDateSpan);
-    dueDateElement.appendChild(document.createTextNode(" - "));
     dueDateElement.appendChild(titleSpan);
 
-    bookElement.appendChild(headingState);
+    // bookElement.appendChild(headingState);
     bookElement.appendChild(libraryNameElement);
     bookElement.appendChild(dueDateElement);
     virtualShelfContainer.appendChild(bookElement);
