9617f92ba887c7772c767b8f530808600ec2ef46
Jason Allen <39307371+jallend1@users.noreply.github.com>
2025-08-15 14:42:17 -0700

    Adds JSDoc commenting to sharedFunctions file

--- sidepanel/sharedFunctions.js
+++ sidepanel/sharedFunctions.js
@@ -1,3 +1,7 @@
+/**
+ * Retrieves the extension version from the manifest and displays it in the UI.
+ * @returns {void}
+ */
 const retrieveExtensionVersion = () => {
   const manifestData = chrome.runtime.getManifest();
   const version = document.querySelector("#version");
